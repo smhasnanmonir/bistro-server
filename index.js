d34af57cb33b34edfadf8bd018e425b319c4305a
Hasnan <monirhasnan@gmail.com>
2023-05-29 14:32:59 +0600

    menu api added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
-dotenv.config();
+require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.send("Boss is sitting");
@@ -14,9 +14,8 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://<username>:<password>@cluster0.pyqmcvy.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pyqmcvy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -36,6 +35,19 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    const menuCollection = client.db("bistroDB").collection("menuCallection");
+
+    app.get("/menu", async (req, res) => {
+      const menu = await menuCollection.find({}).toArray();
+      res.send(menu);
+    });
+
+    app.get("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      const menu = await menuCollection.findOne({ _id: new ObjectId(id) });
+      res.send(menu);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
