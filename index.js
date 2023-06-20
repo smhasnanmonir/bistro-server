fd3ad30edc9fabf15b7258de22400d8313291f70
Hasnan <monirhasnan@gmail.com>
2023-06-20 14:25:47 +0600

    render ready

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+const stripe = require("stripe")(
+  "sk_test_51NGwcgK1rPsA3dPJv7Q6l68B6d94IdTLsCNMEmYn6X3WiTcE8BaomRzVT1UgNq1IaejMwYf2j50nYljZBwYosOp000ylXdXmON"
+);
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
@@ -35,7 +38,8 @@ app.listen(port, () => {
 });
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pyqmcvy.mongodb.net/?retryWrites=true&w=majority`;
+const uri =
+  "mongodb+srv://bistroDB:HxczuEGkoOKUCRGI@cluster0.pyqmcvy.mongodb.net/?retryWrites=true&w=majority;";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -62,15 +66,37 @@ async function run() {
       .collection("reviewCollection");
     const cartCollection = client.db("bistroDB").collection("cartCollection");
     const usersCollection = client.db("bistroDB").collection("usersCollection");
+    const paymentCollection = client
+      .db("bistroDB")
+      .collection("paymentCollection");
 
     //jwt authorization
     app.post("/jwt", (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1h",
-      });
+      const token = jwt.sign(
+        user,
+        "0c6bf1f88db562e4084ee2d9f23dc71b694f0ac04c09411c54df8e03df2bd8bc86d664a0d3951dad8350fbbc41fa9ead0ff7df6aae433cba7edfab84ef9dc367",
+        {
+          expiresIn: "1h",
+        }
+      );
       res.send({ token });
     });
+
+    //verify admin
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
+      }
+      next();
+    };
+
     //menu api
     app.get("/menu", async (req, res) => {
       const menu = await menuCollection.find({}).toArray();
@@ -81,6 +107,20 @@ async function run() {
       const id = req.params.id;
       const menu = await menuCollection.findOne({ _id: new ObjectId(id) });
       res.send(menu);
+    });
+
+    app.post("/menu", async (req, res) => {
+      const newItem = req.body;
+      const result = await menuCollection.insertOne(newItem);
+      res.send(result);
+    });
+
+    //delete menu
+    app.delete("/menu/:id", verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
     });
 
     //review app
@@ -119,20 +159,6 @@ async function run() {
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
-
-    //verify admin
-
-    const verifyAdmin = async (req, res, next) => {
-      const email = req.decoded.email;
-      const query = { email: email };
-      const user = await usersCollection.findOne(query);
-      if (user?.role !== "admin") {
-        return res
-          .status(403)
-          .send({ error: true, message: "forbidden message" });
-      }
-      next();
-    };
 
     //user related api
 
@@ -178,6 +204,44 @@ async function run() {
       const user = await usersCollection.findOne(query);
       const result = { admin: user?.role === "admin" };
       res.send(result);
+    });
+
+    //payment api
+    app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+    // payment data save to database................................................................
+    app.post("/payments", verifyJWT, async (req, res) => {
+      const payment = req.body;
+      const insertResult = await paymentCollection.insertOne(payment);
+
+      const query = {
+        _id: { $in: payment.cartItems.map((id) => new ObjectId(id)) },
+      };
+      const deleteResult = await cartCollection.deleteMany(query);
+
+      res.send({ insertResult, deleteResult });
+    });
+
+    //admin stats
+    app.get("/admin-stats", async (req, res) => {
+      const payments = await paymentCollection.find().toArray();
+      const revenue = payments.reduce((sum, payment) => sum + payment.price, 0);
+      const users = await usersCollection.estimatedDocumentCount();
+      const products = await menuCollection.estimatedDocumentCount();
+      const reviews = await reviewCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+      res.send({ users, products, reviews, orders, revenue });
     });
   } finally {
     // Ensures that the client will close when you finish/error
